d14732f00c7164b1ffe244656da221ae5fe185bd
pewcia <ewelina.serafin996@gmail.com>
2025-04-14 12:00:46 +0200

    Joke generator added in Java Script

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 function showAnswer(response) {
-  alert(response.data.answer);
+  let p = document.querySelector("p");
+  p.innerHTML = "";
+  let joke= response.data.answer;
+
+new Typewriter(p, {
+  autoStart: true,
+  delay: 10,
+  cursor: "",
+})
+.typeString(joke).start ();
 }
 
+function getJoke() {
 let apiKey = "bb203f4a92e7t89f33c44201bd2fbfoa";
-let context =
-  "You are an experience programmer. Please answer my question in the streightforward way. I am a beginner and I want to learn coding";
-let prompt = "What is the one best way to learn coding?";
+let context = "You are an AI assistent that has a great sense of humour.";
+let prompt =
+  "Tell me a funny joke about men.Provide just a joke without any other content"
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 console.log(apiUrl);
 axios.get(apiUrl).then(showAnswer);
+}
+
+function togglestyle() {
+  h1.classList.toggle("highlight");
+}
+
+let p = document.querySelector("p");
+let h1= document.querySelector("h1")
+let buttonElement = document.querySelector("#special-button");
+buttonElement.addEventListener("click", getJoke);
+
+
